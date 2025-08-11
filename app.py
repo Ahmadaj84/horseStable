1ecc62f9e867e6a644d8fd99d83fc7a9bda0e110
Ahmadaj84 <47177105+Ahmadaj84@users.noreply.github.com>
2025-08-11 10:44:43 +0300

    host and port removed

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def home():
     return render_template("index.html", horses=horses, riders=riders, sessions=sessions)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run
