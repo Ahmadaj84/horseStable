c0b434d51a0ce31842732bd8bf89cfc7f0266857
Ahmadaj84 <ahmad.jifry@gmail.com>
2025-08-14 09:56:45 +0300

    horse added
    added horse

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 app = Flask(__name__)
 
 # Get DB connection string from environment variable
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://horsestabledb_user:5mqDX3DRyr1d6fOzcXf9seXOQ9Mp2u1i@dpg-d2cha33uibrs738gnpe0-a.oregon-postgres.render.com/horsestabledb' 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -47,15 +48,19 @@ def add_rider():
 def app_login():
     return render_template("login.html")
 
+@app.route("/testhorse")
+def testhorse():
+    return render_template("testHorse.html")
+
 @app.route("/")
 def home():
-    if 'user_id' not in session:
+    """if 'user_id' not in session:
         flash("Please log in to access this page.", "warning")
-        return redirect(url_for('app_login'))
+        return redirect(url_for('app_login'))"""
     horses = Horse.query.all()
     riders = Rider.query.all()
     sessions = Session.query.all()
     return render_template("index.html", horses=horses, riders=riders, sessions=sessions)
 
 if __name__ == "__main__":
-    app.run
+    app.run#(debug=True)
